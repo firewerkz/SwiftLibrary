1ac02de9b6f380e3f064ba38e2ac0be604c0cc77
Dick <dickdotjohnson@gmail.com>
2020-09-08 11:57:30 +0100

    added iosv13

--- Package.swift
+++ Package.swift
@@ -5,6 +5,8 @@ import PackageDescription
 
 let package = Package(
     name: "MyLibrary",
+    platforms: [.iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -16,7 +18,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         // From here
         // https://swiftpackageregistry.com/firebase/firebase-ios-sdk
-        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", .upToNextMajor(from: "6.28.1"))
+        //.package(url: "https://github.com/firebase/firebase-ios-sdk.git", .upToNextMajor(from: "6.28.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
