b35e738006151683499a902388ab172c41305f1a
Dick <dickdotjohnson@gmail.com>
2021-03-15 10:06:56 +0000

    Added app version check.

---
+++ Sources/MyLibrary/VersionCheck.swift
@@ -0,0 +1,197 @@
+//
+//  VersionCheck.swift
+//  TimetoGo2
+//
+//  Created by Dick Johnson on 27/01/2021.
+//  Copyright © 2021 Dick Johnson. All rights reserved.
+//
+/*
+import Foundation
+
+class LookupResult: Decodable {
+    var results: [AppInfo]
+}
+
+class AppInfo: Decodable {
+    var version: String
+}
+
+func getAppInfo(completion: @escaping (AppInfo?, Error?) -> Void) -> URLSessionDataTask? {
+    guard let identifier = Bundle.main.infoDictionary?["CFBundleIdentifier"] as? String,
+          let url = URL(string: "http://itunes.apple.com/lookup?bundleId=\(identifier)") else {
+            DispatchQueue.main.async {
+                completion(nil, VersionError.invalidBundleInfo)
+            }
+            return nil
+    }
+    let task = URLSession.shared.dataTask(with: url) { (data, _, error) in
+        do {
+            if let error = error { throw error }
+            guard let data = data else { throw VersionError.invalidResponse }
+            let result = try JSONDecoder().decode(LookupResult.self, from: data)
+            guard let info = result.results.first else { throw VersionError.invalidResponse }
+
+            completion(info, nil)
+        } catch {
+            completion(nil, error)
+        }
+    }
+    task.resume()
+    return task
+}
+
+enum VersionError: Error {
+    case invalidBundleInfo, invalidResponse
+}
+
+func checkVersion() {
+    let info = Bundle.main.infoDictionary
+    let currentVersion = info?["CFBundleShortVersionString"] as? String
+    _ = getAppInfo { info, error in
+        if let error = error {
+            print(error)
+        } else if info?.version == currentVersion {
+            print("App is up to date with the App Store")
+        } else {
+            if let currentVersion = currentVersion, let appstoreVersion = info?.version {
+                print("A newer version of this app, \(appstoreVersion), is available on the App Store.\nThe current app version is \(currentVersion)")
+            } else {
+                print("A newer version of this app is available on the App Store")
+            }
+        }
+    }
+}
+*/
+
+// from here https://github.com/acarolsf/checkVersion-iOS
+//
+//  CheckUpdate.swift
+//  CheckApp
+//
+//  Created by Ana Carolina on 13/11/20.
+//  Copyright © 2020 acarolsf. All rights reserved.
+//
+import Foundation
+import UIKit
+
+enum VersionError: Error {
+    case invalidBundleInfo, invalidResponse
+}
+
+class LookupResult: Decodable {
+    var results: [AppInfo]
+}
+
+class AppInfo: Decodable {
+    var version: String
+    var trackViewUrl: String
+}
+
+class CheckUpdate: NSObject {
+
+    static let shared = CheckUpdate()
+
+    func showUpdate(withConfirmation: Bool) {
+        DispatchQueue.global().async {
+            self.checkVersion(force: !withConfirmation)
+        }
+    }
+
+    private  func checkVersion(force: Bool) {
+        if let currentVersion = self.getBundle(key: "CFBundleShortVersionString") {
+            _ = getAppInfo { (info, error) in
+                if let appStoreAppVersion = info?.version {
+                    if let error = error {
+                        print("error getting app store version: ", error)
+                    } else if appStoreAppVersion == currentVersion {
+                        print("Already on the last app version: ", currentVersion)
+                    } else {
+                        print("Needs update: AppStore Version: \(appStoreAppVersion) > Current version: ", currentVersion)
+                        print("appURL \(String(describing: info?.trackViewUrl))")
+                        DispatchQueue.main.async {
+                            if let topController: UIViewController = UIApplication.shared.windows.first?.rootViewController,
+                               let trackViewUrl = info?.trackViewUrl {
+                                topController.showAppUpdateAlert(version: appStoreAppVersion, force: force, appURL: trackViewUrl)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private func getAppInfo(completion: @escaping (AppInfo?, Error?) -> Void) -> URLSessionDataTask? {
+
+        guard let identifier = self.getBundle(key: "CFBundleIdentifier"),
+              let url = URL(string: "http://itunes.apple.com/lookup?bundleId=\(identifier)") else {
+                DispatchQueue.main.async {
+                    completion(nil, VersionError.invalidBundleInfo)
+                }
+                return nil
+        }
+        let task = URLSession.shared.dataTask(with: url) { (data, _, error) in
+
+                do {
+                    if let error = error { throw error }
+                    guard let data = data else { throw VersionError.invalidResponse }
+
+                    let result = try JSONDecoder().decode(LookupResult.self, from: data)
+                    print(result.results)
+                    guard let info = result.results.first else {
+                        throw VersionError.invalidResponse
+                    }
+
+                    completion(info, nil)
+                } catch {
+                    completion(nil, error)
+                }
+            }
+
+        task.resume()
+        return task
+
+    }
+
+    func getBundle(key: String) -> String? {
+
+        guard let filePath = Bundle.main.path(forResource: "Info", ofType: "plist") else {
+          fatalError("Couldn't find file 'Info.plist'.")
+        }
+        // 2 - Add the file to a dictionary
+        let plist = NSDictionary(contentsOfFile: filePath)
+        // Check if the variable on plist exists
+        guard let value = plist?.object(forKey: key) as? String else {
+          fatalError("Couldn't find key '\(key)' in 'Info.plist'.")
+        }
+        return value
+    }
+}
+
+extension UIViewController {
+    @objc fileprivate func showAppUpdateAlert( version: String, force: Bool, appURL: String) {
+        guard let appName = CheckUpdate.shared.getBundle(key: "CFBundleName") else { return } //Bundle.appName()
+        let alertTitle = "New version"
+        let alertMessage = "A new version, \(version), of \(appName) is available on AppStore. Update now!"
+
+        let alertController = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: .alert)
+
+        if !force {
+            let notNowButton = UIAlertAction(title: "Not now", style: .default)
+            alertController.addAction(notNowButton)
+        }
+
+        let updateButton = UIAlertAction(title: "Update", style: .default) { _ in
+            guard let url = URL(string: appURL) else {
+                return
+            }
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            } else {
+                UIApplication.shared.openURL(url)
+            }
+        }
+
+        alertController.addAction(updateButton)
+        self.present(alertController, animated: true, completion: nil)
+    }
+}
